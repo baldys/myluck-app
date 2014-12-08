956dbc1180a0391d8184d6728c62fc4109c63e90
Chris Greenwood <dr.greenwood@gmail.com>
2014-12-07 16:20:03 -0800

    Add pan move events

--- myluck/ViewController.swift
+++ myluck/ViewController.swift
@@ -11,6 +11,51 @@ import UIKit
 class ViewController: UIViewController {
 
 
+    
+
+    // Pan has started. aka choosing a side, top or bottom.
+    // http://stackoverflow.com/questions/6614043/drag-view-from-the-point-you-touch-it-iphone
+    var startPanX:CGFloat = 0
+    var startPanY:CGFloat = 0
+    var startPanDoor = ""
+    @IBAction func PanGestureAction(touch: UIPanGestureRecognizer) {
+        if(touch.state == UIGestureRecognizerState.Began) {
+            
+            //            CGPoint location = [touch locationInView:self.view];
+            //            startX = location.x - ViewMain.center.x;
+            //            startY = ViewMain.center.y;
+            
+            println("Started");
+            
+            var location = touch.locationInView(self.view)
+            startPanX = location.x
+            startPanY = location.y
+            var halfHeight = self.view.bounds.size.height / 2
+            
+            if(startPanY < halfHeight) {
+                startPanDoor = "top"
+            } else {
+                startPanDoor = "bottom"
+            }
+
+        } else if(touch.state == UIGestureRecognizerState.Changed) {
+            // Move the door
+            
+            println(".")
+        } else if(touch.state == UIGestureRecognizerState.Ended) {
+            var halfWidth = self.view.bounds.size.width / 2
+            var distanceX = abs(touch.locationInView(self.view).x - startPanX)
+            if(distanceX > halfWidth) {
+                println("CHOICE LOCKED IN")
+            } else {
+                println("cancelled")
+            }
+
+        }
+        
+    }
+    
+    
     // Listen for swipes left or right
     @IBAction func SwipeLeftAction(sender: UISwipeGestureRecognizer) {
         println("Swiped left")
